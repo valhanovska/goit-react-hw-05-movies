dfe25b289cb3e745b1ea5839348af2427b0e67ac
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-10-03 14:05:16 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ export const App = () => {
             <Route path="reviews" element={<Reviews />} />
           </Route>
 
-          <Route path="*" element={<div>NotFound</div>} />
+          <Route path="*" element={<div>Not Found</div>} />
         </Routes>
       </Suspense>
 
@@ -47,18 +47,3 @@ export const App = () => {
     </>
   );
 };
-
-// '/' – компонент Home, домашня сторінка зі списком популярних
-// кінофільмів.
-
-// '/movies' – компонент Movies, сторінка пошуку кінофільмів за
-// ключовим словом.
-
-// '/movies/:movieId' – компонент MovieDetails, сторінка з
-// детальною інформацією про кінофільм.
-
-// /movies/:movieId/cast – компонент Cast, інформація про
-// акторський склад. Рендериться на сторінці MovieDetails.
-
-// /movies/:movieId/reviews – компонент Reviews, інформація
-// про огляди. Рендериться на сторінці MovieDetails.
